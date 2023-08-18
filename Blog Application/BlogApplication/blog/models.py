a80f9faa38c376230cf7f2d8fa121e4c30814ecf
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-08-18 18:02:14 +0430

    Chapter 2h

--- Blog Application/BlogApplication/blog/models.py
+++ Blog Application/BlogApplication/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
@@ -9,7 +10,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200,unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -25,3 +26,7 @@ class Post(models.Model):
         ]
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    ##it was seo friendly url
+    
