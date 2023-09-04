44e298d3e18baaa70c85e7310e1020228b1f2230
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-09-04 23:41:36 +0430

    Chapter5 Google Authentication

--- Social Website/bookmarks/bookmarks/settings.py
+++ Social Website/bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l4i1qdriy1@ff$r-r&=lv)4%_9w73gfbx6^*fi!ssw8ulu#jib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','mysite.com','loclahost']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -137,4 +139,22 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
+ 'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = 'XXXXX' #Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'XXXXX' #Client Secret
+
+
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+  'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
 ]
