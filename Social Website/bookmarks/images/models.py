74caad5e836b82854ed6d146d92c2c29700295cf
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-09-07 00:04:39 +0430

    Chapter6

--- Social Website/bookmarks/images/models.py
+++ Social Website/bookmarks/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -23,3 +25,6 @@ class Image(models.Model):
         
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
